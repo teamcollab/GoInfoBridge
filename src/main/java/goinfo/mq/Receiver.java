58e91806be7654229a80f38165a80f3f9272f63f
Spooky <smlsun@gmail.com>
2013-12-17 21:50:52 +0800

    test message

--- src/main/java/goinfo/mq/Receiver.java
+++ src/main/java/goinfo/mq/Receiver.java
@@ -1,9 +1,28 @@
 
 package goinfo.mq;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Receiver {
 
 	public void receiveMessage(String message) {
-        System.out.println("Received <" + message + ">");
+
+        Map<String,Object> map = new HashMap<String,Object>();
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            map = mapper.readValue(message,
+                    new TypeReference<HashMap<String,Object>>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println("Received <" + map + ">");
     }
 }
--- src/main/java/goinfo/mq/test/QuerySender.java
+++ src/main/java/goinfo/mq/test/QuerySender.java
@@ -1,9 +1,14 @@
 package goinfo.mq.test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Spooky on 2013/12/17.
@@ -14,12 +19,29 @@ public class QuerySender implements CommandLineRunner {
 
     @Autowired
     RabbitTemplate rabbitTemplate;
+    @Autowired
+    JdbcTemplate jdbcTemplate;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Waiting five seconds...");
         Thread.sleep(5000);
         System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend("spring-boot", "Hello from RabbitMQ! YA");
+
+        Map message = new HashMap<String, Object>();
+
+        message.put("username", "admin");
+
+        message.put("action", "query");  // update, delete, query
+
+        message.put("password", "password");
+        message.put("table", "GHSAHMS1");
+        message.put("params", new HashMap());
+        message.put("queryname", "selectall");
+
+        ObjectMapper mapper = new ObjectMapper();
+        String finalJsonString = mapper.writeValueAsString(message);
+
+        rabbitTemplate.convertAndSend("spring-boot", finalJsonString);
     }
 }
