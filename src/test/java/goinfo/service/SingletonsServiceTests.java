d224615cdac25e33c57501e2b6baab6e805c794b
Spooky <smlsun@gmail.com>
2013-12-23 14:30:52 +0800

    add singletonService test

---
+++ src/test/java/goinfo/service/SingletonsServiceTests.java
@@ -0,0 +1,36 @@
+package goinfo.service;
+
+import goinfo.Application;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.OutputCapture;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Spooky on 2013/12/20.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+@ActiveProfiles("test")
+public class SingletonsServiceTests {
+
+    @Rule
+    public OutputCapture outputCapture = new OutputCapture();
+
+    @Test
+    public void testCommandLineOverrides() throws Exception {
+
+        System.out.println(SingletonsService.getQueriesProperties().getProperty("selectall"));
+
+        String output = this.outputCapture.toString();
+
+        assertTrue(output, output.startsWith("select"));
+}
+
+}
