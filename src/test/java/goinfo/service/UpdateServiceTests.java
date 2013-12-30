8e0d1c96968cefaebe58250e50bcaf17ab10af22
Spooky <smlsun@gmail.com>
2013-12-30 18:47:43 +0800

    add insert test with datetime

--- src/test/java/goinfo/service/UpdateServiceTests.java
+++ src/test/java/goinfo/service/UpdateServiceTests.java
@@ -96,4 +96,30 @@ public class UpdateServiceTests {
         assert result.get("message").equals("受影響的資料共有 1 筆");
 
     }
+
+    @Test
+    public void testInsertManytype(){
+        Map values = new HashMap();
+        values.put("Wcode","002");
+        values.put("Hcode","A10-05F");
+        values.put("Sdate","2012-12-12");
+        values.put("ASno",1);
+
+        Map params =new HashMap<String, String>();
+
+        params.put("username", "admin");
+        params.put("action", "update");
+        params.put("password", "password");
+
+        params.put("queryname", "insertmanytype");
+        params.put("connectname", "major");
+        params.put("values", values);
+
+        Map result = updateService.excute(params);
+
+        assert result.get("success").equals(true);
+        assert result.get("message").equals("受影響的資料共有 1 筆");
+
+    }
+
 }
