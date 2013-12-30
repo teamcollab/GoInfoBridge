efafc50601e2536f8960b202140fb70df5ec57ff
Spooky <smlsun@gmail.com>
2013-12-30 18:51:56 +0800

    add dateconvert

--- src/main/java/goinfo/service/UpdateService.java
+++ src/main/java/goinfo/service/UpdateService.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,9 +32,37 @@ public class UpdateService {
         result.put("success", true);
 
         Map values = new HashMap();
-        if(params.containsKey("values"))
+        if(params.containsKey("values")){
             values = (Map) params.get("values");
 
+            for (Object currentKey : values.keySet())if(values.get(currentKey) != null){
+
+                Object entry = values.get(currentKey);
+
+                System.out.println(entry.toString());
+                Date d = null;
+                try {
+                    d = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss zzz").parse(entry.toString());
+                } catch (ParseException e) {
+                }
+                if (d == null) try {
+                    d = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss").parse(entry.toString());
+                } catch (ParseException e) {
+                }
+
+                if (d == null) try {
+                    d = new SimpleDateFormat("yyyy-MM-dd").parse(entry.toString());
+                } catch (ParseException e) {
+                }
+
+                if (d != null){
+                    entry = new java.sql.Date(d.getTime());
+                    values.put(currentKey, entry);
+                }
+
+            }
+
+        }
         int sqlExcuteResult = 0;
 
         try {
@@ -44,6 +75,6 @@ public class UpdateService {
 
         result.put("message","受影響的資料共有 "+sqlExcuteResult+" 筆");
 
-    return result;
-}
+        return result;
+    }
 }
