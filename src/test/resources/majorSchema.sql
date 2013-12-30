d736244cd724a92618e94f4b04317daad85b2715
Spooky <smlsun@gmail.com>
2013-12-30 18:44:05 +0800

    add date convert update insert

--- src/test/resources/majorSchema.sql
+++ src/test/resources/majorSchema.sql
@@ -2,3 +2,6 @@ CREATE TABLE  TEST_TABLE (Code varchar(100) NOT NULL, Name varchar(100), Note va
 
 insert into TEST_TABLE(Code, Name, Note) values ('A001', 'A001', null);
 insert into TEST_TABLE(Code, Name, Note) values ('A002', 'A002', null);
+
+CREATE TABLE  TEST_TABLE2 (Wcode varchar(100) NOT NULL, Hcode varchar(100), Sdate DATETIME, ASno SMALLINT, PRIMARY KEY (Wcode));
+insert into TEST_TABLE2 (Wcode, Hcode, Sdate, ASno) values ('001', null, now(), null);
