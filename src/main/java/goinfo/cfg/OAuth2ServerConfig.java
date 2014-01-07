b215d667dae7fce52ea36eac2c9ea321b0aa9374
Spooky <smlsun@gmail.com>
2014-01-07 14:08:20 +0800

    step 2

--- src/main/java/goinfo/cfg/OAuth2ServerConfig.java
+++ src/main/java/goinfo/cfg/OAuth2ServerConfig.java
@@ -1,32 +1,34 @@
 package goinfo.cfg;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.core.annotation.Order;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.config.annotation.authentication.configurers.InMemoryClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.OAuth2ServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.OAuth2ServerConfigurer;
+import org.springframework.security.oauth2.provider.error.OAuth2AuthenticationEntryPoint;
 
+@EnableWebSecurity
 @Configuration
 @Order(1)
 public class OAuth2ServerConfig extends OAuth2ServerConfigurerAdapter {
-    private static final String SPARKLR_RESOURCE_ID = "goinfo";
-
+    @Autowired
+    OAuth2AuthenticationEntryPoint oauthAuthenticationEntryPoint;
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
         auth
                 .apply(new InMemoryClientDetailsServiceConfigurer())
                     .withClient("clientId")
-    //                .resourceIds(SPARKLR_RESOURCE_ID)
                     .authorizedGrantTypes("client_credentials")
-    //                .authorities("ROLE_CLIENT")
-    //                .scopes("read","write")
+                    .authorities("ROLE_CLIENT")
+                    .scopes("read", "write")
                     .secret("clientSecret");
     }
 
@@ -50,21 +52,13 @@ public class OAuth2ServerConfig extends OAuth2ServerConfigurerAdapter {
         http
             .authorizeRequests()
                 .antMatchers("/oauth/token").fullyAuthenticated()
-
-                .regexMatchers(HttpMethod.DELETE, "/oauth/users/([^/].*?)/tokens/.*")
-                .access("#oauth2.clientHasRole('ROLE_CLIENT') and (hasRole('ROLE_USER') or #oauth2.isClient()) and #oauth2.hasScope('write')")
-                .regexMatchers(HttpMethod.GET, "/oauth/users/.*")
-                .access("#oauth2.clientHasRole('ROLE_CLIENT') and (hasRole('ROLE_USER') or #oauth2.isClient()) and #oauth2.hasScope('read')")
-                .regexMatchers(HttpMethod.GET, "/oauth/clients/.*")
-                .access("#oauth2.clientHasRole('ROLE_CLIENT') and #oauth2.isClient() and #oauth2.hasScope('read')")
-
-                .antMatchers("/test/sendMsg").hasAnyAuthority("ROLE_USER","SCOPE_TRUST")
+                .antMatchers("/test/sendMsg").authenticated()
+//                .access("#oauth2.clientHasRole('ROLE_CLIENT') and #oauth2.isClient() and #oauth2.hasScope('read')")
 
                 .and()
             .requestMatchers()
-                .antMatchers("/photos/**", "/oauth/token", "/oauth/clients/**", "/oauth/users/**")
+                .antMatchers("/test/sendMsg", "/oauth/token")
                 .and()
-            .apply(new OAuth2ServerConfigurer())
-                .resourceId(SPARKLR_RESOURCE_ID);
+            .apply(new OAuth2ServerConfigurer());
     }
 }
